a944dbcfea1998fe191d008e4d006382e15fdf7c
Otavio Espindola <otavioespindola@gmail.com>
2022-01-22 13:38:35 -0300

    Adicionada: Submeter resposta quando pressionar o Enter

--- script/scripts.js
+++ script/scripts.js
@@ -1,10 +1,20 @@
+
+//declaração de variável fora das funções para que ela possa ser usada em to escopo do código
+let respostaDoUsuario = document.getElementById("inputResposta");
+
+//acionando chamada de resposta com a tecla Enter
+respostaDoUsuario.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        confereResposta();
+    }
+});   
+
 function confereResposta() {
-    respostaCerta = "salsichinha"
-    const respostaDoUsuario = document.getElementById("inputResposta");
+    const respostaCerta = "salsichinha"    
     // uso do método toLowerCase para fazer a resposta ser case insentive
     if (respostaDoUsuario.value.toLowerCase() === respostaCerta.toLowerCase()) {
         // redirecionar para página 2
-        window.location.href = '/pergunta02.html'
+        window.location.href = './pergunta02.html'
     } else {
         alert("Resposta errada, tente outra vez.");
         respostaDoUsuario.value = "";
