de3b262e54912204573e913689195f24d77f3d59
Otavio Espindola <otavioespindola@gmail.com>
2022-01-24 22:25:00 -0300

    Encapsulando funçoes  - Falta ajustar chamada da função ConfereResposta

--- script/scripts.js
+++ script/scripts.js
@@ -1,88 +1,104 @@
+function CarregaPagina(faseAtual) {
+    let arrayDeCharadas = [
+        {
+            id: 35,
+            imagem: "./img/imgPergunta01.jpg",
+            alt: "Um dachshund no mato",
+            texto: "Esse cãozinho é bem popular.",
+            dica: "Tente no diminutivo",
+            titulo: "Pergunta 01",
+            resposta: "salsichinha",
+            fase: 1,
+            proximaFase: 2,
+        },
+        {
+            id: 45,
+            imagem: "./img/imgPergunta02.jpg",
+            alt: "Um prédio muito alto",
+            texto: "Se tivesse unhas",
+            dica: "Hifen e acentos importam",
+            titulo: "Pergunta 02",
+            resposta: "arranha-céu",
+            fase: 2,
+            proximaFase: 3,
 
-//declaração de variável fora das funções para que ela possa ser usada em to escopo do código
-let respostaDoUsuario = document.getElementById("inputResposta");
+        },
+        {
+            id: 55,
+            imagem: "./img/imgpergunta03.jpg",
+            alt: "Um palhaço",
+            texto: "Um palhaço pode ser mal?",
+            dica: "DC",
+            titulo: `Pergunta 03`,
+            resposta: "Coringa",
+            fase: 3,
+            proximaFase: 1,
 
-//acionando chamada de resposta com a tecla Enter
-respostaDoUsuario.addEventListener("keypress", (e) => {
-    if (e.key === "Enter") {
-        confereResposta();
-    }
-});   
+        }
+    ]
+    //filtrando pela código da fase e pegando apenas o objeto encontrado
+    let charada = arrayDeCharadas.filter(function (e) {
+        return e.fase == faseAtual;
+    })
+    charada = charada[0];
+    AtualizaImagem(charada.imagem, charada.alt);
+    AtualizaTexto(charada.titulo, charada.texto, charada.dica);
+    return charada;
 
-// ESSA FUNÇÃO NÃO ESTÁ FUNCIONANDO - falta descobrir como alterar a variável faseAtual dentro da função
-function confereResposta() {        
-    // uso do método toLowerCase para fazer a resposta ser case insentive
-    if (respostaDoUsuario.value.toLowerCase() === respostaDaFase.toLowerCase()) {
-        // redirecionar para pergunta 2
-        faseAtual = charada.proximaFase;        
+}
+function AtualizaImagem(source, txtAlt) {
+    let img = document.getElementById('imagem');
+    img.setAttribute('src', source);
+    img.setAttribute('alt', txtAlt);
+}
+function AtualizaTexto(txtTitulo, txtPergunta, txtdica){
+    let perguntaDaFase = document.getElementById('texto');
+    let tituloDaFase = document.getElementById('titulo')
+    let dicaDaFase = document.getElementById('dica');
+
+    tituloDaFase.innerHTML = txtTitulo;
+    perguntaDaFase.innerHTML = txtPergunta
+    dicaDaFase.innerHTML = txtdica;
+}
+function LimpaResposta() {
+    let inputDeRespostaDoUsuario = document.getElementById('inputResposta')
+    inputDeRespostaDoUsuario.value = "";
+    inputDeRespostaDoUsuario.focus();
+}
+
+function ConfereResposta(charada, respostaDoUsuario) {
+    LimpaResposta();
+    if (respostaDoUsuario.toLowerCase() === charada.resposta.toLowerCase()){
+        return CarregaPagina(charada.proximaFase)
     } else {
         alert("Resposta errada, tente outra vez.");
-        respostaDoUsuario.value = "";
-        respostaDoUsuario.focus();
-    }    
+        return charada;
+    }  
 }
-// array com as propriedades da página
-let arrayDeCharadas = [
-    {
-        id: 35,
-        imagem: "./img/imgPergunta01.jpg",
-        alt: "Um dachshund no mato",
-        texto: "Esse cãozinho é bem popular.",
-        dica: "Tente no diminutivo",
-        titulo: "Pergunta 01",
-        resposta: "salsichinha",
-        fase: 1,
-        proximaFase: 2,
-    },
-    {
-        id: 45,
-        imagem: "./img/imgPergunta02.jpg",
-        alt: "Um prédio muito alto",
-        texto: "Se tivesse unhas",
-        dica: "Hifen e acentos importam",
-        titulo: "Pergunta 02",
-        resposta: "arranha-céu",
-        fase: 2,
-        proximaFase: 3,
 
-    },
-    {
-        id: 55,
-        imagem: "./img/imgpergunta03.jpg",
-        alt: "Um palhaço",
-        texto: "Um palhaço pode ser mal?",
-        dica: "DC",
-        titulo: `Pergunta 03`,
-        resposta: "Coringa",
-        fase: 3,
-        proximaFase: 1,
-
-    }
-]
 
 let faseAtual = 3;
+CarregaPagina(faseAtual);
 
-//filtrando pela código da fase, que será também o fator que ordenará em que aparecerá no game
-let charada = arrayDeCharadas.filter(function (e) {
-    return e.fase == faseAtual;
-})
-charada = charada[0];
 
-//atualizando a imagem
-let imagemDaFase = document.getElementById('imagem');
-imagemDaFase.setAttribute('src', charada.imagem)
-imagemDaFase.setAttribute('alt', charada.alt)
 
-// atualizando o texto
-let textoDaFase = document.getElementById('texto');
-textoDaFase.innerHTML = charada.texto;
-let dicaDaFase = document.getElementById('dica');
-dicaDaFase.innerHTML = charada.dica;
-//atualizando a respota
-let respostaDaFase = charada.resposta;
 
-let tituloDaFase = document.getElementById('titulo');
-tituloDaFase.innerHTML = charada.titulo;
+
+// -----------------------------------------
+
+// //declaração de variável fora das funções para que ela possa ser usada em to escopo do código
+// let respostaDoUsuario = document.getElementById("inputResposta").value;
+
+//acionando chamada de resposta com a tecla Enter
+// function Enter(respostaDoUsuario){
+//     respostaDoUsuario.addEventListener("keypress", (e) => {
+//     if (e.key === "Enter") {
+//         confereResposta(charada);
+//     }
+// });
+// }
+
+
 
 
 
